c43b231efe3a30b770a87d95454da0c819254199
kalaiarasi2981 <kalai77076@gmail.com>
2026-05-30 19:08:27 +0530

    Changes to be committed: 	modified:   src/App.jsx 	modified:   src/main.jsx 	modified:   vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,72 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  HashRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import Home from "./pages/Home";
 import AdminLogin from "./pages/AdminLogin";
 import UserLogin from "./pages/UserLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 import UserDashboard from "./pages/UserDashboard";
 import ProtectedRoute from "./pages/ProtectedRoute";
-import SalesView from "./pages/SalesView"
-import Settings from "./pages/Settings"
+import SalesView from "./pages/SalesView";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/user-login" element={<UserLogin />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/User-dashboard" element={<UserDashboard />} />
-        <Route path="/sales-view" element={<SalesView />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/user-dashboard" element={
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/admin-login"
+          element={
+            <AdminLogin />
+          }
+        />
+
+        <Route
+          path="/user-login"
+          element={
+            <UserLogin />
+          }
+        />
+
+        <Route
+          path="/admin-dashboard"
+          element={
+            <AdminDashboard />
+          }
+        />
+
+        <Route
+          path="/sales-view"
+          element={
+            <SalesView />
+          }
+        />
+
+        <Route
+          path="/settings"
+          element={
+            <Settings />
+          }
+        />
+
+        <Route
+          path="/user-dashboard"
+          element={
             <ProtectedRoute>
-            <UserDashboard />
+              <UserDashboard />
             </ProtectedRoute>
           }
         />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
