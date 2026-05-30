475b0655912a308135f287009a320a39488bc25e
kalaiarasi2981 <kalai77076@gmail.com>
2026-05-30 18:46:25 +0530

    router change to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+import {
+  HashRouter,
+} from "react-router-dom";
+
+import App from "./App";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
